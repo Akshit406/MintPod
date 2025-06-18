877dcd78ffb769fb367fd94f0009545e83cdd4e4
Akshit406 <akshitsaxena450@gmail.com>
2025-06-18 13:57:52 +0530

    App file setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import CreateNFT from './pages/CreateNFT';
+import Community from './pages/Community';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="create" element={<CreateNFT />} />
+          <Route path="community" element={<Community />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
