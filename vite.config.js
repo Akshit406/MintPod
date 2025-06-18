c0bee2da1c09d9420baefbb099b876feaa406c69
Akshit406 <akshitsaxena450@gmail.com>
2025-06-18 13:43:35 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/index.css";`
+      }
+    }
+  },
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: 4096, // 4kb
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', '@heroicons/react']
+  }
 })
