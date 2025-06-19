768d20b1a8db9543e6a70d03747b46fae03226ab
Akshit406 <akshitsaxena450@gmail.com>
2025-06-19 16:29:36 +0530

    layout for pages

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -5,7 +6,9 @@ const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow">
+        <Outlet />{children}
+      </main>
       <Footer />
     </div>
   );
