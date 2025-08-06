b01d5b253d47ac63dd96ddcec5fec3800012f0d1
Akshit406 <akshitsaxena450@gmail.com>
2025-08-06 23:45:58 +0530

    layout updated

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -4,13 +4,16 @@ import Footer from '../components/Footer';
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <>
+    <img src='/bgImg2.jpg' className='fixed contain contrast-150 opacity-[2%] w-full h-full -z-10'/>
+    <div className="min-h-screen flex flex-col z-50">
       <Header />
       <main className="flex-grow">
         <Outlet />{children}
       </main>
       <Footer />
     </div>
+  </>
   );
 };
 
